433548da8f762f203631ecc026e0c7ab1faba278
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2014-08-28 16:39:52 +0200

    Fix bug with empty lines

--- Chat.hs
+++ Chat.hs
@@ -5,6 +5,7 @@ where
 
 import Control.Concurrent
 import Control.Concurrent.Chan
+import Control.Exception
 import Control.Monad
 import Data.IP
 import Data.Time.Clock
@@ -39,6 +40,12 @@ instance Show Message where
         let showTime = formatTime defaultTimeLocale "%d.%m, (%H:%M)" t
         in showTime ++ ": " ++ s
 
+
+makeMsg :: String -> IO Message
+makeMsg text =
+    fmap (\time -> Message (time,text)) getCurrentTime
+
+
 readMessage :: IO Message
 readMessage =
     getLine >>= \s ->
@@ -63,6 +70,14 @@ instance Eq ChatClient where
         clHandle cl1 == clHandle cl2
 
 
+addLineToBuffer :: String -> ChatClient -> ChatClient
+addLineToBuffer [] cl = cl
+addLineToBuffer line cl | words line == [] = cl
+                        | otherwise =
+                            cl
+                            { clBuffer = clBuffer cl ++ [line] }
+
+
 getMsg :: ChatClient -> (ChatClient, Maybe String)
 getMsg cl | hasMsg cl = ( cl { clBuffer = (filterEmpty.tail) (clBuffer cl) }
                         , Just ((head.filterEmpty.clBuffer) cl)
@@ -73,6 +88,7 @@ getMsg cl | hasMsg cl = ( cl { clBuffer = (filterEmpty.tail) (clBuffer cl) }
 hasMsg :: ChatClient -> Bool
 hasMsg cl | clBuffer cl == [] = False
           | (filterEmpty.clBuffer) cl == []= False
+          | (words.head.clBuffer) cl == [] = False
           | otherwise = True
 
 
@@ -111,13 +127,28 @@ broadcast cls str =
     (sequence_ . map (send str)) cls >>
     logMsg ("Broadcasted: "++str)
 
+
+mbroadcastMessage :: [ChatClient] -> Maybe String -> IO ()
+mbroadcastMessage _ Nothing = return ()
+mbroadcastMessage cls (Just text) =
+    broadcastMessage cls text
+
+
+broadcastMessage :: [ChatClient] -> String -> IO ()
+broadcastMessage cls text =
+    makeMsg text >>= \msg ->
+    broadcast cls (show msg)
+
+
 mbroadcast :: [ChatClient] -> Maybe String -> IO ()
 mbroadcast _ Nothing = return ()
 mbroadcast cls (Just msg) = broadcast cls msg
 
+
 send :: String -> ChatClient -> IO ()
 send str cl =
     hPutStrLn (clHandle cl) str >> hFlush (clHandle cl)
+
 
 getMessage :: ChatClient -> IO (ChatClient, Message)
 getMessage cli =
@@ -160,8 +191,8 @@ broadcastOne :: [ChatClient] -> ChatClient -> IO ChatClient
 broadcastOne cls cl =
     let others = filter (not.(== cl)) cls
         (newCl, mtext) = getMsg cl
-    in getCurrentTime >>= \time ->
-       mbroadcast others (liftM (\txt -> (clName cl)++ ": " ++ show (Message (time,txt))) mtext) >>
+    in return (fmap ((clName cl++": ")++) mtext) >>= \mmsg ->
+       mbroadcastMessage others mmsg >>
        return newCl
 
 
@@ -179,25 +210,40 @@ disconnectClients cls =
 disconnectClient :: [ChatClient] -> ChatClient -> IO ()
 disconnectClient cls cl =
     send "Disconnecting..." cl >>
-    broadcast cls (clName cl++ " disconnected") >>
+    broadcastMessage cls (clName cl++" disconnected") >>
     hClose (clHandle cl)
+
 
 renameClients :: [ChatClient] -> IO [ChatClient]
 renameClients cls = (sequence.map (renameClient cls)) cls
 
+
+hasRenameCommand :: ChatClient -> Bool
+hasRenameCommand cl
+    | (not.hasMsg) cl = False
+    | otherwise =
+        let (_,mtext) = getMsg cl
+        in Just True == fmap (\txt ->
+                                  assert ((length.words) txt /= 0) ((head.words) txt == "nick")
+                             ) mtext
+
+
+
+
 renameClient :: [ChatClient] -> ChatClient -> IO ChatClient
 renameClient cls cl | (not.hasMsg) cl = return cl
+                    | (not.hasRenameCommand) cl = return cl
 renameClient cls cl =
     let (newCl, mtext) = getMsg cl
         oldName = clName cl
         makeMsg txt = getCurrentTime >>= \time -> return (show (Message (time, txt)))
-    in if (head.words.(maybe "" id)) mtext == "nick"
+    in if (head.words.(maybe "arg" id)) mtext == "nick"
        then let newName = (unwords.tail.words.(maybe "" id)) mtext
             in makeMsg (oldName ++ " -> " ++ newName) >>= \msg ->
                 broadcast cls msg >>
                 return (newCl { clName =  newName })
        else return cl
-       
+
 
 
 installNewConnections :: [ChatClient] -> [ChatClient] -> IO [ChatClient]
@@ -227,10 +273,11 @@ checkClientForInput :: ChatClient -> IO ChatClient
 checkClientForInput cl =
     let hdl = (clHandle cl)
     in hReady (clHandle cl) >>= \avail ->
-       (if avail 
-        then hGetLine (clHandle cl) 
-        else return "") >>= \readString ->
-       return (cl { clBuffer = filterEmpty ((clBuffer cl) ++ [readString]) })
+       if avail
+       then hGetLine (clHandle cl) >>= \line ->
+            return (addLineToBuffer line cl)
+       else return cl
+
 
 
 listner :: Net.Socket -> Chan ChatClient -> IO ()
